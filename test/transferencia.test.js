a779ac29fb1ca8ae70e709eb46f8b6c398668d8a
Henrique Patti <rezendepatti@gmail.com>
2025-07-09 03:04:50 -0300

    adiciona teste de transferência

---
+++ test/transferencia.test.js
@@ -0,0 +1,56 @@
+const request = require("supertest");
+const { expect } = require("chai");
+
+describe("Transferências", () => {
+  describe("POST /transferencias", () => {
+    it("Deve retornar 201 para transferencia com valores igual ou maior que R$ 10.00", async () => {
+      const responseLogin = await request("http://localhost:3000")
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send({
+          username: "julio.lima",
+          senha: "123456",
+        });
+
+      const token = responseLogin.body.token;
+
+      const response = await request("http://localhost:3000")
+        .post("/transferencias")
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 11,
+          token: "in in",
+        });
+
+      expect(response.status).to.equal(201);
+    });
+
+    it("Deve retornar 422 para transferências com valores abaixo de R$ 10.00", async () => {
+      const responseLogin = await request("http://localhost:3000")
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send({
+          username: "julio.lima",
+          senha: "123456",
+        });
+
+      const token = responseLogin.body.token;
+
+      const response = await request("http://localhost:3000")
+        .post("/transferencias")
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 9,
+          token: "in in",
+        });
+
+      expect(response.status).to.equal(422);
+    });
+  });
+});
